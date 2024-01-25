80e19174a1f724dfcc24e42a1615bbc3f7930bb8
Upanisha <120163856+Upanisha10@users.noreply.github.com>
2024-01-25 11:09:08 +0530

    import java.util.*; public class My{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int tc=sc.nextInt();
            for(int i=0;i<tc;i++){
                int a=sc.nextInt();
                int b=sc.nextInt();
                int c=sc.nextInt();
                if((b-c)>=a){
                    System.out.println("YES");
                }
                else{
                    System.out.println("NO");
                }
            }
            }
    }

---
+++ Student_Registration.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class My{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int tc=sc.nextInt();
+        
+        for(int i=0;i<tc;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if((b-c)>=a){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        }
+}
